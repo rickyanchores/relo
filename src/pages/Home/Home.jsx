a9bec9f88c97873e603d35a4477dab453dc87ebc
RickyAnchores <ricky23anchores@gmail.com>
2024-02-09 16:03:10 +0100

    hero done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='Home flex flex-col justify-center items-center min-h-screen'>
-        <div className="heroText h-72 w-screen flex justify-between items-end gap-60 px-6">
-          <div className="title uppercase text-4xl">Premium Smart Watch Mockups</div>
+        <div className="heroText h-72 w-screen flex justify-around items-end p-6">
+          <div className="title uppercase font-thin text-6xl">Premium Smart Watch Mockups</div>
           <div className="indicator text-gray-600 font-mono">
           <svg class="mt-2 h-8 w-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
