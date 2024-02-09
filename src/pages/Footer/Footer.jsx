d40daeebf56ff2d4d8f64c032888bfd245105cb6
RickyAnchores <ricky23anchores@gmail.com>
2024-02-09 13:50:24 +0100

    created hero

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -4,14 +4,14 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 py-8">
-      <div className="container mx-auto text-center">
-        <h2 className="text-white text-2xl font-bold mb-4">Contact Us</h2>
-        <p className="text-gray-400 mb-2">123 Street Name, City, Country</p>
-        <p className="text-gray-400 mb-2">Phone: +123 456 7890</p>
-        <p className="text-gray-400 mb-2">Email: info@example.com</p>
+    <footer className=" bg-white py-8">
+      <div className="container mx-auto text-center grid md:grid-cols-2 grid-cols-1">
+        <h2 className="text-black text-2xl font-bold mb-4">Relo</h2>
+        <p className="text-gray-600 mb-2">London, United Kingdom</p>
+        <p className="text-gray-600 mb-2">Phone: +123 456 7890</p>
+        <p className="text-gray-600 mb-2">Email: info@example.com</p>
         <div className="flex justify-center space-x-4 mt-6">
-          <a href="#" className="text-gray-400 hover:text-white">
+          <a href="#" className="text-gray-600 hover:text-white">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6"
@@ -27,7 +27,7 @@ const Footer = () => {
               />
             </svg>
           </a>
-          <a href="#" className="text-gray-400 hover:text-white">
+          <a href="#" className="text-gray-600 hover:text-white">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6"
@@ -43,7 +43,7 @@ const Footer = () => {
               />
             </svg>
           </a>
-          <a href="#" className="text-gray-400 hover:text-white">
+          <a href="#" className="text-gray-600 hover:text-white">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6"
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='Home flex justify-center items-center min-h-screen'>
-        <div className="title text-6xl">HOME</div>
+    <div className='Home flex flex-col justify-center items-center min-h-screen'>
+        <div className="heroText h-72 w-screen flex justify-between items-end gap-60 px-6">
+          <div className="title uppercase text-4xl">Premium Smart Watch Mockups</div>
+          <div className="indicator text-gray-600 font-mono">
+          <svg class="mt-2 h-8 w-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+        </svg>
+            <p>Inspired by Semplice Design</p>
+          </div>
+        </div>
+        <img className='heroImage p-8' src="https://www.semplice.com/wp-content/uploads/2021/08/smartwatch-supply-store-bg-scaled.jpg" alt="imgHero" />
     </div>
   )
 }
